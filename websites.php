48264232881b99a694087f6bd7a300c5dc7acbad
ploxiz <moscu.alex@gmail.com>
2014-05-09 19:04:29 +0300

    Added Facebook class body

--- websites.php
+++ websites.php
@@ -1,26 +1,40 @@
 <?php
-        $keyWord  = null;
-        // TODO: read $keyWord
-        $keyWord = $keyWord->toURL = function() {
-            // TODO: URL encoding
-        };
+    $keyWord  = null;
+    // TODO: read $keyWord
+    $keyWord = $keyWord->toURL = function() {
+        // TODO: URL encoding
+    };
 
-        class Wiki {
-            public $url = "http://wikipedia.org/";
-            public $name = "Wikipedia";
+    class Wiki {
 
-            public function getParagraph($keyWord) {
-                $url = "http://en.wikipedia.org/w/api.php?format=json&action=query&titles=" .
-                    $keyWord . "&prop=revisions&rvprop=content";
-                // TODO:
-            }
+        public $url = "http://wikipedia.org/";
+        public $name = "Wikipedia";
+
+        public function getParagraph($keyWord) {
+            $url = "http://en.wikipedia.org/w/api.php?format=json&action=query&titles=" .
+                $keyWord . "&prop=revisions&rvprop=content";
+            // TODO:
+        }
+    }
+
+    class Twitter {
+
+        public $url = "http://twitter.com/";
+        public $name = "Twitter";
+
+        public function searchHashTag($keyWord) {
+            $keyUrl = "https://twitter.com/search?q=" . $keyWord . "&src=hash";
+            // TODO:
+        }
+    }
+
+    class Facebook {
+
+        public $url = "http://facebook.com/";
+        public $name = "Facebook";
+
+        public function login($username, $password) {
+
         }
 
-        class Twitter {
-            public $url = "http://twitter.com/";
-            public $name = "Twitter";
-            public function searchHashTag($keyWord) {
-                $keyUrl = "https://twitter.com/search?q=" . $keyWord . "&src=hash";
-                // TODO:
-            }
-        }
+    }
