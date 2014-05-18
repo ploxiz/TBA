0cea9a450dad6d86170fcc88bd6437d3041468d7
ploxiz <moscu.alex@gmail.com>
2014-05-18 18:40:03 +0300

    Changed minor stuff

--- core.js
+++ core.js
@@ -1,57 +1,55 @@
-var keyWord = null;
+$(document).ready(function() {
 
-function getKey() {
-    keyWord = document.getElementById("key").value;
+    var keyWord = null;
+    var wikipediaAPI = null;
+    var twitterAPI = null;
+    var facebookAPI = null;
 
-    document.getElementById("results").innerHTML = httpGet("http://en.wikipedia.org/w/api.php?" +
-        "format=json&action=query&list=search&srsearch=" +
-        keyWord.replace(" ", "+") +
-        "&srprop"); // testing purposes
+    function getData() { // basically the 'main' function.
+        keyWord = document.getElementById("key").value;
+        wikipediaAPI = "http://en.wikipedia.org/w/api.php?" + "format=json&action=query&list=search&srsearch=" +
+            keyWord.replace(" ", "+") + "&srprop";
+        // TODO: twitterAPI.
+        // TODO: facebookAPI.
 
-    // document.getElementById("results").innerHTML = wikipedia.getResults();
-}
-
-function httpGet(Url) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", Url, true);
-    xmlHttp.send();
-    return xmlHttp.responseText;
-}
-
-var wikipedia = {
-    name : "Wikipedia",
-    address : "http://wikipedia.org/",
-
-    getResults : function() {
-        var allResults = httpGet("http://en.wikipedia.org/w/api.php?format=json&action=query&list=search&srsearch=" +
-            keyWord.replace(" ", "+") +
-            "&srprop");
-
-        var allResultsForSlice = allResults; // in order to keep a "clean" variable with the query results.
-
-        var results = null;
-
-        while (allResultsForSlice.search('title":"') != null) {
-
-            var pos = allResultsForSlice.search('title":"') + 9; // index of the first character in the first result.
-
-            allResultsForSlice = allResultsForSlice.slice(pos, allResultsForSlice.length + 1);
-
-            pos = allResultsForSlice.search('"},') - 1; // index of where one of the results ends.
-
-            results.push(allResultsForSlice.slice(0, pos + 1));
-        }
-
-        return results;
+        // TODO: EVERYTHING!
     }
-};
 
-var twitter = {
-    name : "Twitter",
-    address : "http://twitter.com/"
-};
+    function httpGet(URL) {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", URL, true);
+        xmlHttp.send();
+        return xmlHttp.responseText;
+    }
 
-var facebook = {
-    name : "Facebook",
-    address : "http://facebook.com/"
-};
+    var wikipedia = {
+        name: "Wikipedia",
+        address: "http://en.wikipedia.org/",
+
+        getResults: function () {
+            var allResults = httpGet(wikipediaAPI);
+
+            var results = null; // array with the results we actually care about.
+
+            while (allResults.search('title":"') != null) {
+                var pos = allResults.search('title":"') + 9; // index of the first character in the first result.
+                allResults = allResults.slice(pos, allResults.length + 1);
+                pos = allResults.search('"},') - 1; // index of where one of the results ends.
+                results.push(allResults.slice(0, pos + 1));
+            }
+
+            return results;
+        }
+    };
+
+    var twitter = {
+        name: "Twitter",
+        address: "http://twitter.com/"
+    };
+
+    var facebook = {
+        name: "Facebook",
+        address: "http://facebook.com/"
+    };
+
+});
