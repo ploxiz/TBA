6793df459a8ff4963bd7c7f35971f65169a569dd
ploxiz <moscu.alex@gmail.com>
2014-05-20 22:51:57 +0300

    1st step of wikipedia API function update

--- core.js
+++ core.js
@@ -3,9 +3,11 @@ $(document).ready(function() {
     var keyWord = null;
 
     $("#submit").click(function() { // basically the 'main' function.
+
         keyWord = $("#key").val();
 
         wikipedia.callWikipediaAPI(keyWord.replace(" ", "_"));
+
     });
 
     var wikipedia = {
@@ -13,8 +15,14 @@ $(document).ready(function() {
         address: "http://en.wikipedia.org/",
 
         callWikipediaAPI : function(page) {
-            $.getJSON("http://en.wikipedia.org/w/api.php?action=parse&format=json&callback=?", function(data, textStatus) {
-                $("#results").html(textStatus); // TODO: data retine tot continutul paginii, practic in el gasim informatiile care ne intereseaza
+            $.getJSON("http://en.wikipedia.org/w/api.php?action=parse&format=json&callback=?", {
+                page: page,
+                prop: text,
+                redirects: false, // handles redirects
+                section: 1
+            }).done(function(data, textStatus) {
+                $("#results").html(textStatus + "<br />")
+                             .append(toFinalForm(data));
             })
         }
     };
@@ -28,5 +36,16 @@ $(document).ready(function() {
         name: "Facebook",
         address: "http://facebook.com/"
     };
+
+    // Wikipedia functions.
+    function toFinalForm(data) {
+        // TODO: Sterge tot astfel incat sa ramana doar primul paragraf.
+
+
+    }
+
+    // Twitter functions
+
+    // Facebook functions
 
 });
