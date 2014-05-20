b666f032307d99422d16aa854674ae295f9d154e
ploxiz <moscu.alex@gmail.com>
2014-05-20 17:34:38 +0300

    1st step of wikipedia API function

--- core.js
+++ core.js
@@ -1,44 +1,21 @@
 $(document).ready(function() {
 
     var keyWord = null;
-    var wikipediaAPI = null;
-    var twitterAPI = null;
-    var facebookAPI = null;
 
-    function getData() { // basically the 'main' function.
-        keyWord = document.getElementById("key").value;
-        wikipediaAPI = "http://en.wikipedia.org/w/api.php?" + "format=json&action=query&list=search&srsearch=" +
-            keyWord.replace(" ", "+") + "&srprop";
-        // TODO: twitterAPI.
-        // TODO: facebookAPI.
+    $("#submit").click(function() { // basically the 'main' function.
+        keyWord = $("#key").val();
 
-        // TODO: EVERYTHING!
-    }
-
-    function httpGet(URL) {
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open("GET", URL, true);
-        xmlHttp.send();
-        return xmlHttp.responseText;
-    }
+        wikipedia.callWikipediaAPI(keyWord.replace(" ", "_"));
+    });
 
     var wikipedia = {
         name: "Wikipedia",
         address: "http://en.wikipedia.org/",
 
-        getResults: function () {
-            var allResults = httpGet(wikipediaAPI);
-
-            var results = null; // array with the results we actually care about.
-
-            while (allResults.search('title":"') != null) {
-                var pos = allResults.search('title":"') + 9; // index of the first character in the first result.
-                allResults = allResults.slice(pos, allResults.length + 1);
-                pos = allResults.search('"},') - 1; // index of where one of the results ends.
-                results.push(allResults.slice(0, pos + 1));
-            }
-
-            return results;
+        callWikipediaAPI : function(page) {
+            $.getJSON("http://en.wikipedia.org/w/api.php?action=parse&format=json&callback=?", function(data, textStatus) {
+                $("#results").html(textStatus); // TODO: data retine tot continutul paginii, practic in el gasim informatiile care ne intereseaza
+            })
         }
     };
 
