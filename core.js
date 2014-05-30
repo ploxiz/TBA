f5a37ab861170452f97fc482ba0ebc148833800c
ploxiz <moscu.alex@gmail.com>
2014-05-30 23:49:51 +0300

    Attempt on Twitter API.

--- core.js
+++ core.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     var keyWord = null;
 
     // click the submit button in case of pressing the ENTER key.
-    $("#keyword").keypress(function(e){
+    $("#keyword").keypress(function(e) {
         if(e.keyCode == 13)
             $("#submit-button").click();
     });
@@ -13,15 +13,16 @@ $(document).ready(function() {
     $("#submit-button").click(function() {
         keyWord = $("#keyword").val();
         wikipedia.callWikipediaAPI(keyWord.replace(" ", "_"));
+        twitter.callTwitterAPI();
     });
 
     var wikipedia = {
         name: "Wikipedia",
         address: "http://en.wikipedia.org/",
 
-        callWikipediaAPI : function(page) {
+        callWikipediaAPI : function(pageName) {
             $.getJSON("http://en.wikipedia.org/w/api.php?action=parse&format=json&callback=?", {
-                    page: page,
+                    page: pageName,
                     prop:"text",
                     redirects: false
                 }).done(function(data) {
@@ -35,7 +36,25 @@ $(document).ready(function() {
 
     var twitter = {
         name: "Twitter",
-        address: "http://twitter.com/"
+        address: "http://twitter.com/",
+
+        callTwitterAPI : function () {
+            var base64BearerTokenCredentials = "SGloTUxpelFxMTRNcVhOajJaWExoazJ1TDpxQVE3V2V6UDF1ZWtiamFBQXo1c2hUUFhU" +
+                "VHNUZnJvdnhZU2lyM0xtbzdwSDY1dkR4Nw==";
+
+            $.ajax({
+                type: "POST",
+                url: "https://api.twitter.com/oauth2/token",
+                headers: {
+                    Authorization: "Basic " + base64BearerTokenCredentials,
+                    contentType: "application/x-www-form-urlencoded;charset=UTF-8"
+                },
+                data: "grant_type=client_credentials",
+                success: function(data, textStatus) {
+                    // TODO:
+                }
+            });
+        }
     };
 
 });
