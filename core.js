c754b5de89075470ecf590e1289e7672ad316df7
ploxiz <moscu.alex@gmail.com>
2014-05-30 16:41:56 +0300

    Started the design.

--- core.js
+++ core.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 
     var keyWord = null;
 
-    $("#submit").click(function() { // basically the 'main' function.
-        keyWord = $("#key").val();
+    $("#submit-button").click(function() { // basically the 'main' function.
+        keyWord = $("#keyword").val();
         wikipedia.callWikipediaAPI(keyWord.replace(" ", "_"));
     });
 
@@ -13,25 +13,27 @@ $(document).ready(function() {
 
         callWikipediaAPI : function(page) {
             $.getJSON("http://en.wikipedia.org/w/api.php?action=parse&format=json&callback=?", {
-                page: page,
-                prop:"text",
-                redirects: false
-            }).done(function(data) {
-                var readData = $("<div>" + data.parse.text["*"] + "</div>");
-                var paragraph = readData.find("p:first");
-                $("#wiki_content").html(paragraph);
+                    page: page,
+                    prop:"text",
+                    redirects: false
+                }).done(function(data) {
+
+                    var readData = $("<div>" + data.parse.text["*"] + "</div>");
+                    var paragraph = readData.find("p:first");
+
+                    $("#wikipedia-content").html(paragraph);
             })
         }
+    };
+
+    var reddit = {
+        name: "Reddit",
+        address: "http://reddit.com/"
     };
 
     var twitter = {
         name: "Twitter",
         address: "http://twitter.com/"
-    };
-
-    var facebook = {
-        name: "Facebook",
-        address: "http://facebook.com/"
     };
 
 });
